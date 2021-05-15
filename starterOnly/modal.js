78908825c5419331de239f6ba58c4e06d8544a1e
Maxim <developement.future@gmail.com>
2021-05-15 13:01:30 +0200

    mise en ordre commentaires

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,7 +1,8 @@
-/* ◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊ */
-/* ----------------INITIALISATION VARIABLE---- */
-/* ◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊ */
+/* ººººººººººººººººººººººººººººººººººººººººººº */
+/*                     INITIALISATION VARIABLE */
+/* ººººººººººººººººººººººººººººººººººººººººººº */
 
+// Create ERROR MESSAGE :
 const noGoodFirst = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
 const noGoodLast = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
 const noGoodEmail = "Veuillez entrer un email valide.";
@@ -9,10 +10,14 @@ const noGoodDate = "Veuillez entrer une date de naissance valide.";
 const noGoodQty = "Veuillez entrer une valeur numérique valide.";
 const noGoodCity = "Veuillez choisir une ville.";
 
+// Create REGEX :
+const emailValid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const dateValid = /^\d{4}-\d{2}-\d{2}$/;
 
-/* ◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊ */
-/* ----------------DOM ELEMENTS--------------- */
-/* ◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊ */
+
+/* ººººººººººººººººººººººººººººººººººººººººººº */
+/*                                DOM ELEMENTS */
+/* ººººººººººººººººººººººººººººººººººººººººººº */
 
 // Importation DOM modal Elements
 const modalbg = document.querySelector(".bground");
@@ -45,15 +50,29 @@ const missLocation = document.getElementById("missLocation");
 const closeConf = document.querySelector(".closeConfirmation");
 const confirmationbg = document.getElementById("confirmation");
 
-// Create REGEX :
-const emailValid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const dateValid = /^\d{4}-\d{2}-\d{2}$/;
+
+
+/* ººººººººººººººººººººººººººººººººººººººººººº */
+/*                                      EVENTS */
+/* ººººººººººººººººººººººººººººººººººººººººººº */
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Close modal
+formClose.forEach((elt) => elt.addEventListener("click", closeModal));
+
+// on récupère l'évènement "event" au click du boutton "submit"
+btnSubmit.addEventListener("click", (e) => {
+    e.preventDefault();
+    onClickSubmit(e);
+})
 
 
 
-/* ◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊ */
-/* ----------------FUNCTIONS------------------ */
-/* ◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊ */
+/* ººººººººººººººººººººººººººººººººººººººººººº */
+/*                                   FUNCTIONS */
+/* ººººººººººººººººººººººººººººººººººººººººººº */
 
 // Start modal
 function editNav() {
@@ -74,6 +93,9 @@ function launchModal() {
 function closeModal() {
     modalbg.style.display = "none";
 }
+
+
+/* ----- CHECK INPUT FUNCTION ----- */
 
 // check if input last and first are bigger than 2
 const checkInputNames = () => {
@@ -114,11 +136,12 @@ const checkInputQty = () => {
     if (qty.value >= 1 && qty.value <= 99) {
         console.log("Bonne quantité");
     } else {
-        console.log("Veuillez entrer une valeur numérique valide.")
-        missQuantity.innerHTML += "Veuillez entrer une valeur numérique valide.";
+        console.log(noGoodQty)
+        missQuantity.innerHTML += noGoodQty;
     }
 }
 
+// check if location is checked with this condition
 const checkInputCities = () => {
     var location = "";
     for (var i = 0; i < location.length; i++) {
@@ -139,9 +162,10 @@ const checkInputCgu = () => {
 
 }
 
+/* ----- VALIDATION FUNCTION ----- */
+
 // Form main validation
 const onClickSubmit = () => {
-
     checkInputNames();
     checkInputEmail();
     checkInputDate();
@@ -149,92 +173,3 @@ const onClickSubmit = () => {
     checkInputCities();
     // checkInputCgu();
 }
-
-
-
-/* ◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊ */
-/* ----------------EVENTS--------------------- */
-/* ◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊ */
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// Close modal
-formClose.forEach((elt) => elt.addEventListener("click", closeModal));
-
-// on récupère l'évènement "event" au click du boutton "submit"
-btnSubmit.addEventListener("click", (e) => {
-    e.preventDefault();
-    onClickSubmit(e);
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Valid first input
-// function textValidInput() {
-//     if (first.value === '' && first.lenght > 1) {
-//         alert("Veuillez remplir le champ Prénom");
-//         return false;
-//     }
-// }
-
-// // Valid last input
-// function textValidInput() {
-//     if (last.value === '' && last.lenght > 1) {
-//         alert("Veuillez remplir le champ Nom");
-//         return false;
-//     }
-// }
-
-// // Valid email input
-// function textValidInput() {
-//     if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email.value)) {
-//         alert("Veuillez remplir le champ email");
-//         return false;
-//     }
-// }
-
-// Valid modal form
-// function validfModal() {
-//     if (first == null) {
-//         console.log("Faut finir de remplir le formulaire");
-//     } else if (last == null) {
-//         console.log("Faut finir de remplir le formulaire");
-//     } else if (email == null) {
-//         console.log("Faut finir de remplir le formulaire");
-//     } else if (birthdate == null) {
-//         console.log("Faut finir de remplir le formulaire");
-//     } else if (formData == null) {
-//         console.log("Faut finir de remplir le formulaire");
-//     } else {
-//         console.log("Formulaire validé ! Bravo");
-//     }
-// }
